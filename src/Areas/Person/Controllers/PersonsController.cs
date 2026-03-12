5e0dbfce13b6d3737a56124181789b30e48e73eb
Felipe Santos <felipeschimidt85@gmail.com>
2026-03-12 17:34:13 +0000

    add model persons

--- src/Areas/Person/Controllers/PersonsController.cs
+++ src/Areas/Person/Controllers/PersonsController.cs
@@ -1,7 +1,15 @@
 
 using Microsoft.AspNetCore.Mvc;
+using Areas.Person.Models.Persons;
 
 class PersonController : ControllerBase
 {
-    
+    public PersonController(){
+        
+    }
+
+    public string ListPersons(){
+        return "Lista";
+    }
+
 }
